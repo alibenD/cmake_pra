3da944e1a28e395e0219a232ce608be6f6ab858b
alibenD <aliben.develop@gmail.com>
2018-04-15 18:37:04 +0800

    Initial commit

---
+++ t3a/lib/hello.cpp
@@ -0,0 +1,10 @@
+/* FileName: hello.cpp
+ * Created Date: 2017.8.17
+ * Author: Aliben
+ */
+
+#include"hello.h"
+
+void HelloFunc(){
+	printf("Hello World\n");
+}
---
+++ t3a/lib/hello.h
@@ -0,0 +1,13 @@
+/* FileName: hello.h
+ * Created Date: 2017.8.17
+ * Author: Aliben
+ */
+
+#ifndef __HELLO_H_
+#define __HELLO_H_
+
+#include<stdio.h>
+
+void HelloFunc();
+
+#endif /* __HELLO_H_ */
